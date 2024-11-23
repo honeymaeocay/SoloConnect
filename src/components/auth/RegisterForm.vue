41e5e8c1354b1a6541b555c811d187fe99c362a0
AngelLeoKim <torotorokuyakim1011@gmail.com>
2024-11-23 14:07:33 +0800

    feat: added validators changes in login register app

--- src/components/auth/RegisterForm.vue
+++ src/components/auth/RegisterForm.vue
@@ -1,16 +1,101 @@
+<script setup>
+import {
+  requiredValidator,
+  emailValidator,
+  passwordValidator,
+  confirmedValidator,
+} from '@/utils/validators'
+import { ref } from 'vue'
+
+const formDataDefault = {
+  firstname: '',
+  lastname: '',
+  email: '',
+  password: '',
+  password_confirmation: '',
+}
+
+const formData = ref({
+  ...formDataDefault,
+})
+
+const isPasswordVisible = ref(false)
+const isPasswordConfirmVisible = ref(false)
+const refVForm = ref()
+
+const onSubmit = () => {
+  alert(formData.value.email)
+}
+
+const onFormSubmit = () => {
+  refVForm.value?.validate().then(({ valid }) => {
+    if (valid) onSubmit()
+  })
+}
+</script>
+
 <template>
-  <v-form fast-fail @submit.prevent>
-    <v-text-field label="Firstname" variant="outlined"></v-text-field>
+  <v-form ref="refVForm" fast-fail @submit.prevent="onFormSubmit">
+    <v-row>
+      <v-col cols="12" md="6">
+        <v-text-field
+          v-model="formData.firstname"
+          label="Firstname"
+          :rules="[requiredValidator]"
+        ></v-text-field>
+      </v-col>
+    </v-row>
 
-    <v-text-field label="Lastname" variant="outlined"></v-text-field>
+    <v-row>
+      <v-col cols="12" md="6">
+        <v-text-field
+          v-model="formData.lastname"
+          label="Lastname"
+          :rules="[requiredValidator]"
+        ></v-text-field>
+      </v-col>
+    </v-row>
 
-    <v-text-field label="Address" variant="outlined"></v-text-field>
+    <v-row>
+      <v-col cols="12">
+        <v-text-field
+          v-model="formData.email"
+          label="Email"
+          :rules="[requiredValidator, emailValidator]"
+        ></v-text-field>
+      </v-col>
+    </v-row>
 
-    <v-text-field label="Email Address" variant="outlined"></v-text-field>
+    <v-row>
+      <v-col cols="12" md="6">
+        <v-text-field
+          v-model="formData.password"
+          prepend-icon="mdi-lock"
+          label="Password"
+          :type="isPasswordVisible ? 'text' : 'password'"
+          :append-inner-icon="isPasswordVisible ? 'mdi-eye-off' : 'mdi-eye'"
+          @click:append-inner-icon="isPasswordVisible = !isPasswordVisible"
+          :rules="[requiredValidator, passwordValidator]"
+        ></v-text-field>
+      </v-col>
+    </v-row>
 
-    <v-text-field label="Password" variant="outlined" type="password"></v-text-field>
-
-    <v-text-field label="Password Confirmation" variant="outlined" type="password"></v-text-field>
+    <v-row>
+      <v-col cols="12" md="6">
+        <v-text-field
+          v-model="formData.password_confirmation"
+          prepend-icon="mdi-lock"
+          label="Password Confirmation"
+          :type="isPasswordVisible ? 'text' : 'password'"
+          :append-inner-icon="isPasswordVisible ? 'mdi-eye-off' : 'mdi-eye'"
+          @click:append-inner-icon="isPasswordVisible = !isPasswordConfirmVisible"
+          :rules="[
+            requiredValidator,
+            confirmedValidator(formData.password_confirmation, formData.password),
+          ]"
+        ></v-text-field>
+      </v-col>
+    </v-row>
 
     <v-btn class="mt-2" type="submit" block color="warning" prepend-icon="mdi-account-plus"
       >Register</v-btn
