6398d0da249b77ed59b122b6d5ab3736cd16fa5f
Rey Niel <nielnielfelisilda@gmail.com>
2024-12-06 23:33:43 +0800

    feat: added supabase

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -5,11 +5,3 @@ export const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
   import.meta.env.VITE_SUPABASE_ANON_KEY,
 )
-
-// Form Action utils
-export const formActionDefault = {
-  formProcess: false,
-  formStatus: 200,
-  formErrorMessage: '',
-  formSucessMessage: ''
-}
